b5ed8c886c47a01b211359ba5171a0d91588874a
Tucker Bradford <tucker@tuckerbradford.com>
2022-05-29 17:26:41 -0400

    First Prod ready

--- src/extension.ts
+++ src/extension.ts
@@ -4,17 +4,19 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	let disposable = vscode.commands.registerCommand('wikify.makeWiki', () => {
 		const fileName = vscode.window.activeTextEditor?.document?.fileName;
-		if (!fileName) {return}
+		if (!fileName) {return};
 		const baseName = fileName.split('/').reverse()[0];
-		const destFilename = `/Users/mtuckerb/workspace/mtuckerb/mtuckerb.github/${baseName}`;
-		fs.link( fileName, destFilename, (err: any) => {
+		const config = vscode.workspace.getConfiguration('wikify');
+		const targetDir = `${config.targetDir}/`;
+
+		fs.link( fileName, `${targetDir}${baseName}`, (err: any) => {
 			if (err) {
-				vscode.window.showInformationMessage(`Couldn't link ${fileName}: ${err}`);
+				vscode.window.showInformationMessage(`Couldn't create link: ${err}`);
 			} else {
-				vscode.window.showInformationMessage(`Created a hard link from ${fileName} to ${destFilename}!`);
+				vscode.window.showInformationMessage(`Created a hard link from ${fileName} to ${targetDir}!`);
 			}
 
-		})
+		});
 	});
 
 	context.subscriptions.push(disposable);
